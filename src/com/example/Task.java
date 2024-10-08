253f4f2310224d9aacdc260e6fe579592e14baac
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-10-08 16:34:36 +0200

    First commit

---
+++ src/com/example/Task.java
@@ -0,0 +1,36 @@
+package com.example;
+
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    // Getters y setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
